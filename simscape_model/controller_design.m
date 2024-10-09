176100918f16fe88973066a37cdc4bd64307bf21
KazupD <kazup.daniel@gmail.com>
2024-10-09 19:40:53 +0200

    Matlab and simulink modifications

--- simscape_model/controller_design.m
+++ simscape_model/controller_design.m
@@ -2,7 +2,6 @@ clear; clc;
 
 Cm = 0.1;
 J_shaft = 0.012;
-Ja = 0.001;
 La = 0.047;
 Ra = 0.5;
 b_shaft = 2.21;
@@ -28,10 +27,27 @@ D = [0];
 
 sys = ss(A, B, C, D);
 
-s1 = -0.5;
-s2 = -1.08e+01
-s3 = -1.01e+02
-s4 = -4.21e+01 + 1.29e+02i
+figure;
+pzmap(sys);
+% Get the handle to the current plot
+hAx = gca;
+% Set the marker size for poles and zeros
+% Get the handles to the poles and zeros markers
+hMarkers = findobj(hAx, 'Type', 'line');
+% Loop through each marker and set the marker size for poles and zeros
+for i = 1:numel(hMarkers)
+    if any(hMarkers(i).XData) % Check if the marker represents a pole or zero
+        set(hMarkers(i), 'MarkerSize', 10); % change size
+    end
+end
+
+pole(sys)
+damp(sys)
+
+s1 = -1.0
+s2 = (-1.08e+01)
+s3 = (-1.01e+02)
+s4 = (-4.21e+01 + 1.29e+02i)
 s5 = conj(s4)
 
 phic=[s1 s2 s3 s4 s5];
@@ -41,9 +57,9 @@ K=acker(A,B,phic)
 At = A-B*K
 
 Kr = -inv(C*inv(At)*B)
-
+ 
 sys_c = ss(At, B, C, D)
-
+ 
 figure;
 step(4*sys_c*Kr);
 
